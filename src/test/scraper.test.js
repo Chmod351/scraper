20fa08d105d99d098694001fa42369314cfc0948
yamil351 <leokasper@live.com.ar>
2023-03-08 15:46:46 -0300

    tests-pass

--- src/test/scraper.test.js
+++ src/test/scraper.test.js
@@ -3,41 +3,43 @@ const { expect } = require("chai");
 const myRequest = request("http://localhost:5000/api");
 
 describe("scraping websites", () => {
-  it("its should return an result with statuscode 200 /api/scraper/", async () => {
-    const testScrap = {
-      url: "https://www.lanacion.com.ar/",
-      objectClass: ".com-link",
-      keyWord: "la",
-    };
-    const response = await myRequest.post("/scraper").send(testScrap);
-    expect(response.statusCode).to.be.equal(200);
+  describe("status code 400", () => {
+    it("should return statusCode 400 bad request /api/scraper/", async () => {
+      const testScrap = {
+        url: "",
+        objectClass: ".post",
+        keyWord: "linux",
+      };
+      const response = await myRequest.post("/scraper").send(testScrap);
+      expect(response.text).to.be.equal('{"message":"bad request"}');
+    });
+    it("should return statusCode 400 bad request /api/scraper", async () => {
+      const testScrap = {
+        url: "",
+        objectClass: "",
+        keyWord: "",
+      };
+      const response = await myRequest.post("/scraper").send(testScrap);
+      expect(response.statusCode).to.be.equal(400);
+    });
   });
-  it("its should return an json with an error message/api/scraper/", async () => {
-    const testScrap = {
-      url: "",
-      objectClass: ".com-link",
-      keyWord: "la",
-    };
-    const response = await myRequest.post("/scraper").send(testScrap);
-    expect(response.text).to.be.equal(
-      '{"error":{"status":404,"message":"Not Found"}}'
-    );
+  describe("status code 200", () => {
+    it("should return statuscode 200 /api/scraper/", async () => {
+      const testScrap = {
+        url: "https://libreddit.de/r/linux",
+        objectClass: ".post_title",
+        keyWord: "linux",
+      };
+      const response = await myRequest.post("/scraper").send(testScrap);
+      expect(response.statusCode).to.be.equal(200);
+    });
+    it("should return statusCode 200 /api/scraper", async () => {
+      const testScrap = {
+        url: "https://libreddit.de/r/linux",
+        objectClass: ".post_title",
+      };
+      const response = await myRequest.post("/scraper").send(testScrap);
+      expect(response.statusCode).to.be.equal(200);
+    });
   });
-  it("should return an json with statusCode 200 /api/scraper", async () => {
-    const testScrap = {
-      url: "https://www.lanacion.com.ar/",
-      objectClass: ".com-link",
-    };
-    const response = await myRequest.post("/scraper").send(testScrap);
-    expect(response.statusCode).to.be.equal(200);
-  });
- it('should return an error with 400 bad request', async()=>{
-   const testScrap = {
-      url: "",
-      objectClass: "",
-    };
-    const response = await myRequest.post("/scraper").send(testScrap);
-    expect(response.statusCode).to.be.equal(400);
-
- })
 });
