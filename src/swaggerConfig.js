a1e0197fda4106e307b90efdd383ea54e7771ca9
yamil351 <leokasper@live.com.ar>
2023-03-09 14:28:50 -0300

    link-git-add

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -7,7 +7,7 @@ const options = {
       title: "API de Web Scraper",
       version: "1.0.0",
       description:
-        "Suppose you want some information from a website? Let’s say a paragraph on Donald Trump! What do you do? Well, you can copy and paste the information from Wikipedia to your own file. But what if you want to get large amounts of information from a website as quickly as possible? Such as large amounts of data from a website to train a Machine Learning algorithm? In such a situation, copying and pasting will not work! And that’s when you’ll need to use Web Scraping",
+        "Suppose you want some information from a website? Let’s say a paragraph on Donald Trump! What do you do? Well, you can copy and paste the information from Wikipedia to your own file. But what if you want to get large amounts of information from a website as quickly as possible? Such as large amounts of data from a website to train a Machine Learning algorithm? In such a situation, copying and pasting will not work! And that’s when you’ll need to use Web Scraping. You can watch more about the project here https://github.com/yamilt351/scraper",
       license: {
         name: "GPL",
         url: "https://github.com/yamilt351/scrape",
