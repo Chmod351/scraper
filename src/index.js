c0b0fad886174445f3bf5ae02ec62673b30572c6
yamil351 <leokasper@live.com.ar>
2023-07-18 13:49:43 -0300

    port deleted

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const options = require('./swaggerConfig');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const options = require("./swaggerConfig");
 const app = express();
-const scrape = require('./routes/scraper.js');
-const ratelimit = require('express-rate-limit');
-const morgan = require('morgan');
-const port = 5000;
+const scrape = require("./routes/scraper.js");
+const ratelimit = require("express-rate-limit");
+const morgan = require("morgan");
+const port = process.env.PORT;
 const path = require('path');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const specs = swaggerJsdoc(options);
 
 const corsOptions = {
   origin: [
-    'https://scraper-5ask.onrender.com/api/docs/#/default/post_api_scrape',
-    'https://scraper-5ask.onrender.com',
-    'https://scraper-5ask.onrender.com/api/docs',
+    "https://scraper-5ask.onrender.com/api/docs/#/default/post_api_scrape",
+    "https://scraper-5ask.onrender.com",
+    "https://scraper-5ask.onrender.com/api/docs",
   ],
-  allowedHeaders: 'Content-Type',
+  allowedHeaders: "Content-Type",
 };
 
 const limit = ratelimit({
@@ -31,18 +31,18 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-  morgan(':method :url :status :response-time ms - :res[content-length]'),
+  morgan(":method :url :status :response-time ms - :res[content-length]")
 );
 app.use(limit);
 //END MIDDLEWARES
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use('/api', scrape);
+app.use("/api", scrape);
 app.use(
-  '/api/docs',
+  "/api/docs",
   cors(corsOptions),
   swaggerUi.serve,
-  swaggerUi.setup(specs),
+  swaggerUi.setup(specs)
 );
 
 app.listen(port, function () {
