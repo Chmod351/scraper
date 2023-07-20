1f88f0e79035435a3d9a5457e8f52312efb4d227
yamil351 <leokasper@live.com.ar>
2023-07-20 14:18:02 -0300

    error handlers

---
+++ src/helpers/errorHandler.js
@@ -0,0 +1,30 @@
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NotFoundError';
+    this.statusCode = 404;
+  }
+}
+
+
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'BadRequestError';
+    this.statusCode = 400;
+  }
+}
+
+function errorHandler(error, req, res, next) {
+  if (error.statusCode) {
+    res.status(error.statusCode).json({ error: error.message });
+  }
+  next();
+}
+export {
+  errorHandler,
+  UnauthorizedError,
+  BadRequestError,
+  AuthenticationError,
+  NotFoundError,
+};
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import swaggerUi from 'swagger-ui-express';
 import importMiddlewares from './config/middlewaresConfig.js';
 import { fileURLToPath } from 'url';
 import config from './config/envConfig.js';
+import { errorHandler } from './helpers/errorHandler.js';
 
 // CONFIG
 const specs = swaggerJsdoc(options);
@@ -24,7 +25,7 @@ middlewares.forEach((middleware) => {
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/api', scrape);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs));
-
+app.use(errorHandler);
 app.listen(port, function () {
   console.log(`the aplication is running on http://localhost:${port}`);
 });
