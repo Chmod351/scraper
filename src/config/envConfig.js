b02501bfeef5a9f2321524f0c85ccd64b633fe7a
yamil351 <leokasper@live.com.ar>
2023-08-01 08:32:29 -0300

    postgresql credentials

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -4,4 +4,7 @@ export default {
   port: process.env.PORT,
   host: process.env.URL_SERVER,
   requestLimit: process.env.LIMIT,
+  database: process.env.POSGRESQL_DATABASE,
+  databaseUsername: process.env.POSGRESQL_USERNAME,
+  databasePassword: process.env.POSGRESQL_PASSWORD,
 };
