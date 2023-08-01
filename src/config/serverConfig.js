a6d4dd552cf0a823dfbef6e81bc5ef1433b1758b
yamil351 <leokasper@live.com.ar>
2023-08-01 12:18:04 -0300

    credentials add

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,8 +1,13 @@
 import { Sequelize } from 'sequelize';
 import envConfig from './envConfig.js';
-const server = new Sequelize('scrapper', 'username', 'password', {
-  host: envConfig.host,
-  dialect: 'postgres',
-});
+const server = new Sequelize(
+  envConfig.database,
+  envConfig.databaseUsername,
+  envConfig.databasePassword,
+  {
+    host: envConfig.host,
+    dialect: 'postgres',
+  },
+);
 
 export default server;
