a644b6db3b042c3ff5a7b63f851e9f5af9ab5f7c
sudeepmahato16 <sudeepmahato16@gmail.com>
2023-08-12 16:08:22 +0545

    Add scroll indicator

---
+++ src/public/js/scroll-indicator.js
@@ -0,0 +1,13 @@
+const handleScroll = () => {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+};
+
+window.onscroll = () => {
+  handleScroll();
+};
