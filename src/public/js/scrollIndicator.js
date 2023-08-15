1634601c2e1a79f9bd043a3b833953c7b685ce82
yamil351 <leokasper@live.com.ar>
2023-08-15 18:21:35 -0300

    Feature: Now The url change dinamcly depends the section id

--- src/public/js/scrollIndicator.js
+++ src/public/js/scrollIndicator.js
@@ -5,9 +5,42 @@ const handleScroll = () => {
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
   const scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  document.getElementById('myBar').style.width = scrolled + '%';
 };
+
+function updateUrlFragment(sectionId) {
+  if (history.replaceState) {
+    history.replaceState(null, null, `#${sectionId}`);
+  } else {
+    window.location.hash = sectionId;
+  }
+}
+
+function getCurrentSection() {
+  const sections = document.querySelectorAll('section');
+  let currentSection = null;
+
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (
+      rect.top <= window.innerHeight / 2 &&
+      rect.bottom >= window.innerHeight / 2
+    ) {
+      currentSection = section.id;
+    }
+  });
+
+  return currentSection;
+}
+
+function handleScrollIndicator() {
+  const currentSection = getCurrentSection();
+  if (currentSection) {
+    updateUrlFragment(currentSection);
+  }
+}
 
 window.onscroll = () => {
   handleScroll();
+  handleScrollIndicator();
 };
