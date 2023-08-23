482cbf9183c70d0cb2540c3e877f0579ee47ee0b
yamil351 <leokasper@live.com.ar>
2023-08-23 13:25:35 -0300

    Test: related data edge cases test

--- src/test/relatedData.unit.test.js
+++ src/test/relatedData.unit.test.js
@@ -14,4 +14,26 @@ describe('Positive cases: Integration Test', () => {
     );
     expect(response.statusCode).to.be.equal(200);
   });
+  it('should not return related results that contain the keyword', async () => {
+    const body = {
+      keyWord: 'dólar',
+    };
+    const response = await myRequest.get('/related').send(body);
+    expect(response.statusCode).to.be.equal(200);
+    expect(response.body).to.be.an('array').that.is.not.empty;
+
+    const keyword = body.keyWord.toLowerCase();
+
+    response.body.forEach((item) => {
+      expect(item.title.toLowerCase()).to.contain(keyword);
+    });
+  });
+  it('should return an empty array', async () => {
+    const body = {
+      keyWord: 'casasdasd',
+    };
+    const response = await myRequest.get('/related').send(body);
+    expect(response.statusCode).to.be.equal(200);
+    expect(response.body).to.deep.equal([]);
+  });
 });
