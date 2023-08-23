cfc494908bc422f5c199442a9047a7f78d59ec36
yamil351 <leokasper@live.com.ar>
2023-08-23 17:04:25 -0300

    Fix: Typo

--- src/test/relatedData.integration.test.js
+++ src/test/relatedData.integration.test.js
@@ -1,8 +1,8 @@
 import Request from 'supertest';
 import { expect } from 'chai';
-// import envConfig from '../config/envConfig';
-// const baseUrl = `${envConfig.host}:${envConfig.port}/api/v1`;
-const baseUrl = 'https://scraper-5ask.onrender.com/api/v1';
+import envConfig from '../config/envConfig';
+const baseUrl = `${envConfig.host}:${envConfig.port}/api/v1`;
+// const baseUrl = 'https://scraper-5ask.onrender.com/api/v1';
 
 const myRequest = Request(baseUrl);
 
